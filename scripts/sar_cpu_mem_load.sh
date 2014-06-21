a8d1365f111ae88f0ec6937225733ed2ff0a4826
stayhigh <stayhigh@github.com>
2014-06-21 14:03:49 -0400

    check the sar command if exists before report it

--- scripts/sar_cpu_mem_load.sh
+++ scripts/sar_cpu_mem_load.sh
@@ -1,4 +1,15 @@
-echo "producing cpu.csv mem.csv load.csv"
-sar -u 1 10 > ./cpu.csv &
-sar -r 1 10 > ./mem.csv &
-sar -q 1 10 > ./load.csv &
+which sar
+
+if test $? -ne 0
+then
+  tput setf 4
+  echo "[failed] sar not existed"
+  tput sgr0
+else
+  tput setf 2
+  echo "producing cpu.csv mem.csv load.csv"
+  tput sgr0
+  sar -u 1 10 > ./cpu.csv &
+  sar -r 1 10 > ./mem.csv &
+  sar -q 1 10 > ./load.csv &
+fi
