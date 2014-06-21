70713a98613eaa939f40bc378d13a37ab3e7e94f
stayhigh <stayhigh@github.com>
2014-06-21 14:06:39 -0400

    sar_cpu_mem_load.sh return code list added

--- scripts/sar_cpu_mem_load.sh
+++ scripts/sar_cpu_mem_load.sh
@@ -1,3 +1,9 @@
+#which command return code list
+#EXIT STATUS
+#0      if all specified commands are found and executable
+#1      if one or more specified commands is nonexistent or not executable
+#2      if an invalid option is specified
+
 which sar
 
 if test $? -ne 0
