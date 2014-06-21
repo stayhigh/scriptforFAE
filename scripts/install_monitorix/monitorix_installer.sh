4e89e945a44ac11418ef33c7ec8dba9ae8482817
stayhigh <stayhigh@github.com>
2014-06-21 14:39:38 -0400

    add repo URL in source.list if not exists, and overwrite GPG keys if asc file exists

---
+++ scripts/install_monitorix/monitorix_installer.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+grep "^deb http://apt.izzysoft.de/ubuntu generic universe" /etc/apt/sources.list
+if test $? -ne 0
+then
+  echo "adding repository URL into /etc/apt/source.list"
+  echo "deb http://apt.izzysoft.de/ubuntu generic universe" >> /etc/apt/sources.list
+else
+  echo "repository URL already in /etc/apt/source.list"
+fi
+
+wget http://apt.izzysoft.de/izzysoft.asc -O izzysoft.asc
+echo "overwrite izzysoft.asc if exists"
+apt-key add izzysoft.asc
+sudo apt-get update
+sudo apt-get -y install monitorix
