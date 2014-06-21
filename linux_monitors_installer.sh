fbc8f4496522830d4224bedbe1b26119e35034ac
stayhigh <stayhigh@github.com>
2014-06-21 13:39:42 -0400

    installer for linux monitors

---
+++ linux_monitors_installer.sh
@@ -0,0 +1,27 @@
+#install 20 Command Line Tools to Monitor Linux Performance
+#http://www.tecmint.com/command-line-tools-to-monitor-linux-performance/
+#VmStat – Virtual Memory Statistics
+#Iotop – Monitor Linux Disk I/O
+#Iostat – Input/Output Statistics:devices, local disks, remote disks
+#IPTraf – Real Time IP LAN Monitoring
+#Psacct or Acct – Monitor User Activity
+#Monit – Linux Process and Services Monitoring
+#NetHogs – Monitor Per Process Network Bandwidth
+#iftop – Network Bandwidth Monitoring
+#Monitorix – System and Network Monitoring
+#Arpwatch – Ethernet Activity Monitor (arp spoofing detecting)
+#Nmon: Monitor Linux Performance
+
+sudo apt-get -y install tcpdump
+sudo apt-get -y install netstat
+sudo apt-get -y install htop 
+sudo apt-get -y install iftop 
+sudo apt-get -y install nethogs
+sudo apt-get -y install nmon
+sudo apt-get -y install collectl 
+sudo apt-get -y install iptraf
+sudo apt-get -y install acct
+sudo apt-get -y install monit
+sudo apt-get -y install arpwatch
+sudo apt-get -y install vnstat
+sudo apt-get -y install sysstat
