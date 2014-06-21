68960dfa10a42ca9ea2b9a71c3219c4af09b4c56
stayhigh <stayhigh@github.com>
2014-06-21 12:33:36 -0400

    indent all code

--- pingtest.sh
+++ pingtest.sh
@@ -17,35 +17,35 @@
 
 
 ping_localhost(){
-echo "ping localhost 5 times ..."
-ping -q -c5 localhost > /dev/null
-if [ $? -eq 0 ]
-then
-        tput setf 2
-	echo "[ok] ping localhost"
-	tput sgr0
-else
-        tput setf 4
-	echo "[failed] ping localhost"
-	tput sgr0
+  echo "ping localhost 5 times ..."
+  ping -q -c5 localhost > /dev/null
+  if [ $? -eq 0 ]
+  then
+          tput setf 2
+          echo "[ok] ping localhost"
+          tput sgr0
+  else
+          tput setf 4
+          echo "[failed] ping localhost"
+          tput sgr0
 
-fi
+  fi
 }
 
 ping_google(){
-echo "ping google.com 5 times ..."
-ping -q -c5 google.com > /dev/null
- 
-if [ $? -eq 0 ]
-then
-        tput setf 2
-	echo "[ok] ping google.com"
-	tput sgr0
-else
-        tput setf 4
-	echo "[failed] ping google.com"
-	tput sgr0
-fi
+  echo "ping google.com 5 times ..."
+  ping -q -c5 google.com > /dev/null
+   
+  if [ $? -eq 0 ]
+  then
+          tput setf 2
+          echo "[ok] ping google.com"
+          tput sgr0
+  else
+          tput setf 4
+          echo "[failed] ping google.com"
+          tput sgr0
+  fi
 }
 
 ping_localhost
