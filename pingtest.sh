3f79f98686109eba33d2bc4933f6712e883a3d2d
stayhigh <stayhigh@github.com>
2014-06-21 12:18:23 -0400

    [added]  pingtest script for local and google.com

---
+++ pingtest.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+ 
+# -q quiet
+# -c nb of pings to perform
+#tput:  2 for green, 4 for red color
+ 
+
+echo "ping localhost 5 times ..."
+ping -q -c5 google.com > /dev/null
+if [ $? -eq 0 ]
+then
+        tput setf 2
+	echo "[ok] ping localhost"
+	tput sgr0
+else
+        tput setf 4
+	echo "[failed] ping localhost"
+	tput sgr0
+
+fi
+
+echo "ping google.com 5 times ..."
+ping -q -c5 google.com > /dev/null
+ 
+if [ $? -eq 0 ]
+then
+        tput setf 2
+	echo "[ok] ping google.com"
+	tput sgr0
+else
+        tput setf 4
+	echo "[failed] ping google.com"
+	tput sgr0
+fi
