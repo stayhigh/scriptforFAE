a85beaf9295ae37c071babccd16c4027a4480ddd
stayhigh <stayhigh@github.com>
2014-06-22 11:43:00 -0400

    cd to script diretory, and to copy procedures

--- scripts/github_settings/set_all_config.sh
+++ scripts/github_settings/set_all_config.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+SCRIPT_ABSPATH=$(readlink -f $0)
+SCRIPTDIR=$(dirname $SCRIPT_ABSPATH)
+echo script path: $SCRIPT_ABSPATH
+echo NOW SWITCH INTO: $SCRIPTDIR
+cd $SCRIPTDIR
 cp .gitconfig ~/.gitconfig
+echo cp .gitconfig ~/.gitconfig
 cp .netrc ~/.netrc
+echo cp .netrc ~/.netrc
 
