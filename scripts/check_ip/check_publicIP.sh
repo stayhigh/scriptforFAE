9a84ebad4797ecdbdd1d9c48713ea9af84725212
stayhigh <stayhigh@github.com>
2014-06-28 11:27:37 -0400

    check_publicIP.sh updated external sites

--- scripts/check_ip/check_publicIP.sh
+++ scripts/check_ip/check_publicIP.sh
@@ -12,6 +12,12 @@ check_public_ip ipv4.icanhazip.com
 check_public_ip ident.me
 check_public_ip v4.ident.me
 check_public_ip http://smart-ip.net/myip
+check_public_ip whatismyip.akamai.com
+check_public_ip tnx.nl/ip
+check_public_ip myip.dnsomatic.com
+check_public_ip ip.appspot.com
+check_public_ip ip.telize.com
+check_public_ip curlmyip.com
 
 
 #check IPv6
