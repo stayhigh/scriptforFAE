0687f1b234933978db688ce03d1b2d8bcf416f77
stayhigh <stayhigh@github.com>
2014-07-03 09:55:50 -0400

    update watch command example

---
+++ scripts/monitoring/watch_command.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#show the average of loading on the system per 10 secs
+watch -n 10 'cat /proc/loadavg'
+#watch the commandline per second
+watch -n 1 -d=cumulative tail targetfile.txt
+
+#monitoring the number of http connections
+watch -n 1 -d 'pstree|grep http'
+
+#monitoring the change of file
+watch -d 'ls -l|grep targetfile.txt'
