c178bec617c60ca724932994842c568e9c146025
stayhigh <stayhigh@github>
2015-02-02 08:49:04 +0800

    unrar sample scripts

---
+++ scripts/compress/unrar_commands.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#uncompress rar file
+unrar x some.rar
+
+#uncompress rar file (exclude directories)
+unrar e some.rar
+
+#list all files in rar
+unrar l some.rar
+
+#test rar file 
+unrar t some.rar
