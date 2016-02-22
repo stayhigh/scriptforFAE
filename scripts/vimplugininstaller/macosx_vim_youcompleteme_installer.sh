f5eac742819caf0cadd647ab7b850b86a5810711
john.wang <john.wang@gandi.net>
2016-02-22 13:33:51 +0800

    refactoring the script and remove the vim symlink

--- scripts/vimplugininstaller/macosx_vim_youcompleteme_installer.sh
+++ scripts/vimplugininstaller/macosx_vim_youcompleteme_installer.sh
@@ -2,13 +2,20 @@
 
 #reference: https://github.com/Valloric/YouCompleteMe#mac-os-x-installation
 
-#symlink vim and install brew 
-ln -s /usr/local/bin/mvim vim 
-which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#install brew 
+
+function installbrew(){
+    which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+}
 
 #install cmake if it has not been installed
-brew info cmake > /dev/null || brew install cmake
+function installcmake(){
+    brew info cmake > /dev/null || brew install cmake
+}
 
 #Compiling YCM without semantic support for C-family languages:
-cd ~/.vim/bundle/YouCompleteMe && sh install.sh
+function compilingYCM(){
+    cd ~/.vim/bundle/YouCompleteMe && sh install.sh
+}
 
+installbrew && installcmake && compilingYCM
