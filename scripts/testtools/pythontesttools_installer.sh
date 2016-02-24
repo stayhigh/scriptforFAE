97593c327360830d85097bf2a489b08fe367f1be
john.wang <john.wang@gandi.net>
2016-02-24 14:38:33 +0800

    python test tools installer

---
+++ scripts/testtools/pythontesttools_installer.sh
@@ -0,0 +1,8 @@
+#  reference: http://docs.python-guide.org/en/latest/writing/tests/
+
+#  python testing tools 
+pip install nose #  nose extends unittest to make testing easier.
+pip install pytest #  py.test is a no-boilerplate alternative to Python’s standard unittest module.
+pip install tox #  tox is a tool for automating test environment management and testing against multiple interpreter configurations
+pip install unittest2 #  unittest2 is a backport of Python 2.7’s unittest module which has an improved API and better assertions over the one available in previous versions of Python.
+pip install mock #  unittest.mock is a library for testing in Python. As of Python 3.3, it is available in the standard library.
