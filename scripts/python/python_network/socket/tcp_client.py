1ff74bea4132930017b2911c3d244aa05f3cfe9c
john.wang <john.wang@gandi.net>
2016-02-24 23:51:56 +0800

    python tcp socket programs

---
+++ scripts/python/python_network/socket/tcp_client.py
@@ -0,0 +1,32 @@
+import socket   #for sockets
+import sys  #for exit
+ 
+host = 'localhost'
+port = 8888
+
+try:
+    #create an AF_INET, STREAM socket (TCP)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error, msg:
+    print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+    sys.exit();
+finally:
+	print 'Socket Created'
+ 
+ 
+try:
+    remote_ip = socket.gethostbyname( host )
+except socket.gaierror:
+    #could not resolve
+    print 'Hostname could not be resolved. Exiting'
+    sys.exit()
+finally:
+    print 'IP address of ' + host + ' is ' + remote_ip
+ 
+#Connect to remote server
+try:
+	s.connect((remote_ip , port))
+except Exception as e:
+	print e.message, e.args
+finally:
+	print 'Socket Connected to ' + host + ' on ip ' + remote_ip
