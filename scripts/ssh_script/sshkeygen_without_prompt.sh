baf38a51c0e8eff7b7d096344147380dfc287740
stayhigh <redhero0702@gmail.com>
2016-03-02 13:33:23 +0800

    remove the script

--- scripts/ssh_script/sshkeygen_without_prompt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-MACOSX="192.168.136.128"
-MINT="192.168.136.129"
-sudo apt-get -y install sshpass &&
-yes |ssh-keygen -f ~/.ssh/id_rsa  -t rsa -N ''
-ssh-add
-sshpass -p 29906219 sudo ssh-copy-id -i ~/.ssh/id_rsa.pub stayhigh@$MINT
-
