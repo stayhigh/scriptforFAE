d838bb139376f480e64e1b0502f708ee31c819f0
stayhigh <redhero0702@gmail.com>
2016-03-02 13:47:58 +0800

    updated the script for installing ssh tools on mac osx

---
+++ scripts/ssh_script/installHomeBrew_sshTools.sh
@@ -0,0 +1,3 @@
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+brew install ssh-copy-id
+brew install https://raw.github.com/eugeneoden/homebrew/eca9de1/Library/Formula/sshpass.rb
